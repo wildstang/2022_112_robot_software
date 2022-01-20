959ce77bcc51eb41fe073f0dc77c56bd32982c80
foxler2010 <drew@drewssite.top>
2022-01-19 22:28:39 -0600

    add comments

--- src/main/java/org/wildstang/year2022/subsystems/Intake.java
+++ src/main/java/org/wildstang/year2022/subsystems/Intake.java
@@ -14,14 +14,20 @@ import org.wildstang.year2022.robot.WSOutputs;
  */
 public class Intake implements Subsystem {
 
+    //intake motor
     WsSparkMax motor;
+    //a button
     WsJoystickButton forwardTrigger;
+    //y button
     WsJoystickButton backwardTrigger;
 
     @Override
     public void init() {
+        //intake motor
         motor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.INTAKE_MOTOR);
+        //a button
         forwardTrigger = (WsJoystickButton) Core.getInputManager().getInput(WSInputs.DRIVER_FACE_DOWN);
+        //y button
         backwardTrigger = (WsJoystickButton) Core.getInputManager().getInput(WSInputs.DRIVER_FACE_UP);
     }
 
@@ -32,12 +38,17 @@ public class Intake implements Subsystem {
 
     @Override
     public void inputUpdate(Input source) {
+        //if the a button or y button has changed state
         if (source == forwardTrigger || source == backwardTrigger) {
+            //if the a button is currently pressed
             if (forwardTrigger.getValue()) {
+                //spin motor forward at full speed
                 motor.setValue(1);
-            } else if (backwardTrigger.getValue()) {
+            } else if (backwardTrigger.getValue()) { //if the y button is currently pressed
+                //spin motor backward at full speed
                 motor.setValue(-1);
-            } else {
+            } else { //if a or y buttons are NOT currently pressed
+                //stop motor from spinning
                 motor.setValue(0);
             }
         }
