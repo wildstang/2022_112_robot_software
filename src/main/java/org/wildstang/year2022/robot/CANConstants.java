9f1e49e093332fe1e9075fc882ace7d058af9d17
foxler2010 <drew@drewssite.top>
2022-01-19 22:01:31 -0600

    remove everything, add in intake motor

--- src/main/java/org/wildstang/year2022/robot/CANConstants.java
+++ src/main/java/org/wildstang/year2022/robot/CANConstants.java
@@ -9,30 +9,7 @@ package org.wildstang.year2022.robot;
  */
 public final class CANConstants {
 
-    // Replace these examples.
-    // While not independently dangerous if implemented these could have unintended effects.
-    public static final int[] EXAMPLE_PAIRED_CONTROLLERS    = {1,2};
-    public static final int   EXAMPLE_MOTOR_CONTROLLER      = 3;
-
-    //tank drive constants
-    public static final int LEFT_DRIVE = 11;
-    public static final int RIGHT_DRIVE = 12;
-    public static final int LEFT_DRIVE_FOLLOWER = 13;
-    public static final int RIGHT_DRIVE_FOLLOWER = 14;
-
-    //swerve constants
-    public static final int ENC1 = 41;
-    public static final int ENC2 = 42;
-    public static final int ENC3 = 43;
-    public static final int ENC4 = 44;
-    public static final int DRIVE1 = 11;
-    public static final int ANGLE1 = 12;
-    public static final int DRIVE2 = 13;
-    public static final int ANGLE2 = 14;
-    public static final int DRIVE3 = 15;
-    public static final int ANGLE3 = 16;
-    public static final int DRIVE4 = 17;
-    public static final int ANGLE4 = 18;
-    
+    //intake motor
+    public static final int INTAKE_MOTOR = 0;
     
 }
