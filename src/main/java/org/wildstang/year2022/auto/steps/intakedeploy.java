b6253f8efc04bf09bd7eb09d59817147192ca943
Lwannebo5476 <lwannebo5476@stu.d214.org>
2022-03-14 19:23:09 -0500

    things

---
+++ src/main/java/org/wildstang/year2022/auto/steps/intakedeploy.java
@@ -0,0 +1,16 @@
+package org.wildstang.year2022.auto;
+
+import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.framework.auto.steps.PathFollowerStep;
+import org.wildstang.framework.core.Core;
+import org.wildstang.year2022.robot.WSSubsystems;
+import org.wildstang.year2022.subsystems.drive.Drive;
+import frc.paths.*;
+
+
+
+public class intakedeploy {
+    
+
+
+}
